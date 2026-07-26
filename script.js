3ea0b6f6d0868e1169da7700a37ff37e6da935c9
geraldykanilo-transport <geraldykanilo@gmail.com>
2026-07-26 22:50:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+window.addEventListener("scroll",function(){
 
+const header=document.querySelector("header");
+
+if(window.scrollY>50){
+
+header.style.background="#000";
+
+}else{
+
+header.style.background="rgba(0,0,0,.85)";
+
+}
+
+});
+
+console.log("Makassar Executive VIP Transport Loaded");
